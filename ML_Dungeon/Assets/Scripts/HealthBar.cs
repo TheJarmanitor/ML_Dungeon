9d4b8f26a0d3143d6146b85e3cf37e8a0d2c8945
Oleg Jarma Montoya <ojarmam@unal.edu.co>
2020-06-04 18:43:59 -0500

    Add Game Demo

---
+++ ML_Dungeon/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+
+    public int maxHealth=100;
+    public int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth=maxHealth;
+    }
+
+    public void takeDamage(int Damage)
+    {
+        currentHealth-=Damage;
+    }
+}
